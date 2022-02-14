6db376c7fbba32eab770cf43ab0108c79e59fdee
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-02-14 01:13:14 +0100

    routing config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AppRoutingRoutingModule } from './app-routing/app-routing-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,18 +7,18 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent,
-    CreateComponent,
-    EditComponent
-  ],
+  declarations: [AppComponent, ListComponent, CreateComponent, EditComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    AppRoutingRoutingModule,
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
